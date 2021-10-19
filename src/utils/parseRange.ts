e42c4e12b2dfc08aa401275bef2e99bca3628684
Ernest2077 <ernest1637@gmail.com>
2021-10-19 19:10:46 +0600

    MAR-1762 Deleted utc Updated test

--- src/utils/parseRange.ts
+++ src/utils/parseRange.ts
@@ -4,18 +4,18 @@ import moment from 'moment'
 const DEFAULT_FORMAT = 'DD/MM/YYYY'
 
 function getYesterday(): string {
-  return moment().utc().subtract(1, 'day').format(DEFAULT_FORMAT)
+  return moment().subtract(1, 'day').format(DEFAULT_FORMAT)
 }
 
 function getLastWeek(): string {
   const end = getYesterday()
-  const start = moment().utc().subtract(1, 'week').format(DEFAULT_FORMAT)
+  const start = moment().subtract(1, 'week').format(DEFAULT_FORMAT)
   return `${start} - ${end}`
 }
 
 function getLastMonth(): string {
   const end = getYesterday()
-  const start = moment().utc().subtract(1, 'month').format(DEFAULT_FORMAT)
+  const start = moment().subtract(1, 'month').format(DEFAULT_FORMAT)
   return `${start} - ${end}`
 }
 
