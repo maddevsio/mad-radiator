3d92b25a9b17c2299634e0081b31c672c10104ec
Ernest2077 <ernest1637@gmail.com>
2021-10-27 11:44:01 +0600

    feat:MAR-1768 Implemented the radiator modularity

--- example/index.js
+++ example/index.js
@@ -1,5 +1,21 @@
 const { Radiator } = require('../lib')
 const config = require('./example.config.json')
 
-const radiator = new Radiator(config)
+const {
+  authConfig,
+  sentryConfig,
+  analyticsConfig,
+  lighthouseConfig,
+  telegramConfig,
+  slackConfig,
+  scheduleConfig,
+} = config
+
+const radiator = new Radiator(authConfig)
+radiator.useSentry(sentryConfig)
+radiator.useAnalytics(analyticsConfig)
+radiator.useLighthouse(lighthouseConfig)
+radiator.useTelegram(telegramConfig)
+radiator.useSlack(slackConfig)
+radiator.scheduleJob(scheduleConfig)
 radiator.run()
