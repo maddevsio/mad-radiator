639d37da2dc3eaa42a08b46df5831646dd294c4f
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-01-19 11:17:18 +0600

    MAR-2577. Updated test

--- src/chartBuilder/__tests__/ChartBuilder.spec.ts
+++ src/chartBuilder/__tests__/ChartBuilder.spec.ts
@@ -1,31 +1,9 @@
 import { ChartBuilder } from 'chartBuilder/ChartBuilder'
 
-jest.mock('chartjs-node-canvas', () => ({
-  ChartJSNodeCanvas: class {
-    renderToBufferSync = jest.fn()
-
-    constructor({ chartCallback }: { chartCallback: (ChartJS: unknown) => void }) {
-      chartCallback({ defaults: { font: {} } })
-    }
-  },
-}))
-jest.mock('sharp', () => () => ({
-  png: () => ({
-    toBuffer: () => 'buffer',
-  }),
-}))
-
 describe('ChartBuilder', () => {
   it('should correctly create an instance', () => {
     const builder = new ChartBuilder()
 
     expect(builder.renderChart).toBeTruthy()
-  })
-
-  it('should correctly called renderChart method and got png', async () => {
-    const builder = new ChartBuilder()
-    const img = await builder.renderChart({ test: 1, test2: 2 })
-
-    expect(img).toBe('buffer')
   })
 })
