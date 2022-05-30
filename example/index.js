db6da91c895cf1c12655739e8c1673977f234cce
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-05-30 15:46:31 +0600

    MAR-3404. Updated example structure, added Google authorization

--- example/index.js
+++ example/index.js
@@ -1,21 +1,92 @@
 const { Radiator } = require('../lib')
 const config = require('./example.config.json')
+const express = require('express');
+const session = require('express-session');
+require('dotenv').config()
 
-const {
-  authConfig,
-  sentryConfig,
-  analyticsConfig,
-  // scheduleConfig,
-  // telegramConfig,
-  slackConfig,
-  // lighthouseConfig
-} = config
+const passport = require('passport');
+const path = require('path');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
-const radiator = new Radiator(authConfig)
-radiator.useSentry(sentryConfig)
-radiator.useAnalytics(analyticsConfig)
-// radiator.useLighthouse(lighthouseConfig)
-// radiator.useTelegram(telegramConfig)
-radiator.useSlack(slackConfig)
-// radiator.scheduleJob(scheduleConfig)
-radiator.run()
+const app = express();
+
+const CLIENT_ID=process.env.GOOGLE_CLIENT_ID
+const CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET
+const CALLBACK_URL='http://localhost:3000/authorized'
+const PORT=3000
+let userInfo = {}
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser( function(id, done) {
+  done(null, id);
+});
+
+passport.deserializeUser( function(id, done) {
+  done(null, id);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: CALLBACK_URL
+  },
+  async function(accessToken, refreshToken, profile, email, cb) {
+    userInfo.idToken = profile.id_token
+    userInfo.accessToken = accessToken
+    userInfo.refreshToken = refreshToken
+    return cb(null, userInfo);
+  }
+));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/google/auth',
+  passport.authenticate('google', {
+      scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/analytics.readonly'],
+      accessType: 'offline',
+      prompt: 'consent',
+  }),
+    (req, res) => {
+        console.log('GOOGLE AUTH: ', res)
+    }
+);
+
+app.get('/authorized',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    const {
+      authConfig,
+      sentryConfig,
+      analyticsConfig,
+      slackConfig,
+    } = config
+    
+    Object.assign(authConfig, userInfo)
+    
+    const radiator = new Radiator(authConfig)
+    radiator.useSentry(sentryConfig)
+    radiator.useAnalytics(analyticsConfig)
+    radiator.useSlack(slackConfig)
+    radiator.run()
+
+    res.redirect('/');
+  }
+);
+
+app.listen(PORT, () => {
+  console.log('Server is running on Port ' + PORT)
+  const url = `http://localhost:${PORT}`
+  require('child_process')
+      .exec((process.platform
+          .replace('darwin','')
+          .replace(/win32|linux/,'xdg-') + 'open ' + url));
+})
