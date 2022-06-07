04c5930abbbfae91e29e93b8ec59eadab2eac162
oh-cactus <roman.zh@maddevs.io>
2022-06-07 18:03:20 +0300

    Refactoring pageAnalytics components

--- src/pagesAnalytics/PageAnalytics.ts
+++ src/pagesAnalytics/PageAnalytics.ts
@@ -6,9 +6,6 @@ import { Sitemap } from 'sitemap/Sitemap'
 import { SitemapOptions } from '../sitemap/interfaces'
 import { Firestore } from '../utils/firestore'
 
-
-// import { Blog } from './interfaces'
-
 export class PageAnalytics {
   private readonly config: SitemapOptions
 
@@ -25,6 +22,16 @@ export class PageAnalytics {
     this.currentCount = 0
   }
 
+  public async getPageAnalyticsMetrics(): Promise<Object | null> {
+    const { data } = await this.setCountOfBlogPages()
+    if (data) {
+      Logger.success('Count saved!')
+      return this.getCountOfBlogs()
+    }
+    Logger.error('Failed saving on firestore')
+    return null
+  }
+
   public async setCountOfBlogPages(): Promise<any> {
     Logger.info('️Start getting blog links from sitemap')
     const urls = await this.sitemap.getAllUrls()
@@ -33,30 +40,31 @@ export class PageAnalytics {
       this.currentCount = urls.length
       Logger.success(`Count of links from sitemap: ${urls.length}`)
       Logger.info(`Saving count of links on firestore...`)
-      this.firestore.setData('blog', {
+
+      return this.firestore.setData('blog', {
         fields: {
           count: { integerValue: urls.length },
           created: { timestampValue: new Date() }
         }
-      }).then(({ data }) => {
-        Logger.success('Count saved!')
-        console.dir(data, { depth: null, maxArrayLength: null });
-        return this.getPageAnalyticsMetrics()
-      }).catch(() => {
-        Logger.error('Failed saving on firestore')
       })
-    } else {
-      Logger.error('Something went wrong')
     }
+    Logger.error('Something went wrong')
+    return null
   }
 
-  public async getPageAnalyticsMetrics(): Promise<number> {
+  public async getCountOfBlogs(): Promise<object> {
     Logger.info(`Start getting data from firestore`)
+
     const firstDayOfCurrentMonth = moment().startOf('month').toISOString()
     const { data } = await this.firestore.getDataAfterDate(firstDayOfCurrentMonth, 1)
+
     console.dir(data, { depth: null, maxArrayLength: null });
+
     const oldCount = data[0].document?.fields?.count?.stringValue
+
     Logger.info(`Новых статей за месяц: ${this.currentCount - oldCount}`)
-    return this.currentCount - oldCount
+    return {
+      blogPageCount: this.currentCount - oldCount
+    }
   }
 }
--- src/pagesAnalytics/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export * from 'pagesAnalytics/interfaces/Blog'
