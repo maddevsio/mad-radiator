35b58a62c457dac967aff8a2daaa67f6565fa099
Antoneo-MD <anton.zaichenko@maddevs.io>
2022-07-13 13:22:28 +0600

    MAR-3599. removed redundant methods and close todo.

--- src/analytics/EbookDownloadsRepository.ts
+++ src/analytics/EbookDownloadsRepository.ts
@@ -1,5 +1,5 @@
 import { Repository } from 'analytics/Repository'
-import { AnalyticDataRows, AnalyticsPayload, EbookDownloads } from 'analytics/interfaces'
+import { AnalyticDataRows, EbookDownloads } from 'analytics/interfaces'
 
 /**
  * Submit Ebook repository
@@ -31,33 +31,21 @@ export class EbookDownloadsRepository extends Repository {
    */
   public async getData(): Promise<Array<EbookDownloads>> {
     const reports = await this.getAnalytics(this.metrics, this.dimensions, this.dateRanges)
-    reports.rows = reports.rows
-      .filter(
-        (row: AnalyticDataRows) => row.dimensionValues[0]?.value === 'submit_pricing_strategies_ebook_form'
-          || row.dimensionValues[0]?.value === 'submit_approach_dp_ebook_form',
-      )
-    return EbookDownloadsRepository.format(reports)
-  }
-
-  /**
-   * Format raw GA4 data
-   */
-  private static format(reports: AnalyticsPayload): Array<EbookDownloads> {
-    return reports?.rows
-      .map(
-        (row: AnalyticDataRows): EbookDownloads => {
-          let name = '' // todo change to .reduce
-          if (row.dimensionValues[0].value === 'submit_pricing_strategies_ebook_form') {
-            name = 'Pricing Strategies'
-          }
-          if (row.dimensionValues[0].value === 'submit_approach_dp_ebook_form') {
-            name = 'Approach Development Process'
-          }
-          return {
+    return reports.rows
+      .reduce((acc: Array<EbookDownloads>, row: AnalyticDataRows) => {
+        if(row.dimensionValues[0]?.value === 'submit_pricing_strategies_ebook_form'){
+          acc.push({
             value: Number(row.metricValues[0].value),
-            name: String(name),
-          }
-        },
-      )
+            name: 'Pricing Strategies',
+          })
+        }
+        if(row.dimensionValues[0]?.value === 'submit_approach_dp_ebook_form'){
+          acc.push({
+            value: Number(row.metricValues[0].value),
+            name: 'Approach Development Process',
+          })
+        }
+        return acc
+      },<Array<AnalyticDataRows>>[])
   }
 }
