ba9e7b2b46a21c2767f61c39d9c23c7807641e70
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-08-01 16:14:34 +0600

    fix: fix problems with the url for the glassdoor service and make method to send data to enji as a public

--- src/enji/EnjiService.ts
+++ src/enji/EnjiService.ts
@@ -8,13 +8,14 @@ export class EnjiService {
     this.url = url;
   }
 
-  async sendTotalUsersToEnjiWithDate(users: number, analyticsDate: Moment) {
+  public async sendTotalUsersToEnjiWithDate(users: number, analyticsDate: Moment) {
     try {
       const params = {
         count: users,
         date: analyticsDate.format('YYYY-MM-DD'),
       };
       const { data } = await axios.get(this.url, { params });
+      console.log(data);
       return data;
     } catch (error: any) {
       throw new Error(`Cannot send data to Enji: ${error.message}`);
