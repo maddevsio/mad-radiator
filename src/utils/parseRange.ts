b10ca24cd0a150c29ff0fdcc70eb2545a720451e
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-08-05 17:13:33 +0600

    add time

--- src/utils/parseRange.ts
+++ src/utils/parseRange.ts
@@ -5,7 +5,7 @@ import moment, { Moment } from 'moment'
 const DEFAULT_FORMAT = 'DD/MM/YYYY'
 
 function getYesterday(): string {
-  console.log(`getYesterday():\n1. moment().fomat(DEFAULT_FORMAT): ${moment().format(DEFAULT_FORMAT)}\n2. moment().subtract(1, 'day').format(DEFAULT_FORMAT): ${moment().subtract(1, 'day').format(DEFAULT_FORMAT)}\n`)
+  console.log(`getYesterday():\n1. moment().fomat('DD/MM/YYYY h:mm:ss a'): ${moment().format('DD/MM/YYYY h:mm:ss a')}\n2. moment().subtract(1, 'day').format(DEFAULT_FORMAT): ${moment().subtract(1, 'day').format('DD/MM/YYYY h:mm:ss a')}\n`)
   return moment().subtract(1, 'day').format(DEFAULT_FORMAT)
 }
 
@@ -22,7 +22,7 @@ function getLastMonth(): string {
 }
 
 function getTwoDaysAgo(): Moment {
-  console.log(`getTwoDaysAgo(): \n1. moment().format(DEFAULT_FORMAT): ${moment().format(DEFAULT_FORMAT)}\n2. moment().subtract(2, 'days').format(DEFAULT_FORMAT): ${moment().subtract(2, 'days').format(DEFAULT_FORMAT)}\n`)
+  console.log(`getTwoDaysAgo(): \n1. moment().format('DD/MM/YYYY h:mm:ss a'): ${moment().format('DD/MM/YYYY h:mm:ss a')}\n2. moment().subtract(2, 'days').format(DEFAULT_FORMAT): ${moment().subtract(2, 'days').format('DD/MM/YYYY h:mm:ss a')}\n`)
   return moment().subtract(2, 'day')
 }
 
