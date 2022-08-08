74835aa42b6c8b4bb3b36306685888b4c8e21227
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-08-08 15:01:25 +0600

    add a new method to the logger class

--- src/utils/parseRange.ts
+++ src/utils/parseRange.ts
@@ -1,27 +1,28 @@
-/* eslint-disable no-console */
-import getFormattedDate from 'helpers/getFormattedDate'
 import { ParsedRange, RangeType } from 'interfaces'
+import { Logger } from 'logger'
 import moment, { Moment } from 'moment'
 
+const DEFAULT_FORMAT = 'DD/MM/YYYY'
+
 export function getYesterday(): string {
-  console.log(`getYesterday():\n1. moment().fomat('DD/MM/YYYY h:mm:ss a'): ${moment().format('DD/MM/YYYY h:mm:ss a')}\n2. moment().subtract(1, 'day').format(DEFAULT_FORMAT): ${moment().subtract(1, 'day').format('DD/MM/YYYY h:mm:ss a')}\n`)
-  return getFormattedDate('day')
+  Logger.logMessageDate('getYesterday', 'day', 1, 'DD/MM/YYYY h:mm:ss a')
+  return moment().subtract(1, 'day').format(DEFAULT_FORMAT)
 }
 
 function getLastWeek(): string {
   const end = getYesterday()
-  const start = getFormattedDate('week')
+  const start = moment().subtract(1, 'week').format(DEFAULT_FORMAT)
   return `${start} - ${end}`
 }
 
 function getLastMonth(): string {
   const end = getYesterday()
-  const start = getFormattedDate('month')
+  const start = moment().subtract(1, 'month').format(DEFAULT_FORMAT)
   return `${start} - ${end}`
 }
 
 export function getTwoDaysAgo(): Moment {
-  console.log(`getTwoDaysAgo(): \n1. moment().format('DD/MM/YYYY h:mm:ss a'): ${moment().format('DD/MM/YYYY h:mm:ss a')}\n2. moment().subtract(2, 'days').format(DEFAULT_FORMAT): ${moment().subtract(2, 'days').format('DD/MM/YYYY h:mm:ss a')}\n`)
+  Logger.logMessageDate('getTwoDaysAgo', 'days', 2, 'DD/MM/YYYY h:mm:ss a')
   return moment().subtract(2, 'day')
 }
 
