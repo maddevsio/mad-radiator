b41972dcd4edd6cb73cd446a8af69ad1e403a041
vladDomino <vladislav.k@maddevs.io>
2022-09-06 13:35:15 +0600

    MAR-3772. Created helpers utils.

---
+++ src/utils/executeWithRetry.ts
@@ -0,0 +1,29 @@
+import { Logger } from 'logger'
+import { sleep } from './sleep'
+
+export async function executeWithRetry(
+  prompt: string,
+  retryCount: number,
+  sleepMs: number,
+  successCb: () => any,
+  errorCb: (error: any) => any,
+) {
+  Logger.info(`${prompt}: Starting.`)
+  for (let attempt = 1; attempt <= retryCount; attempt += 1) {
+
+    function promptStr(): string {
+      return `${prompt} attempt ${attempt}:`
+    }
+
+    try {
+      if (attempt > 1) Logger.info(`${promptStr()} Trying getting data.`)
+      const result = await successCb()
+      if (attempt > 1) Logger.info(`${promptStr()} Success.`)
+      return result
+    } catch (error: any) {
+      Logger.error(`${promptStr()} ${error}`)
+      if (attempt > retryCount - 1 || !errorCb(error)) throw error
+      await sleep(sleepMs)
+    }
+  }
+}
